2cdab38b100f4fe6dd80ba3f19066d7bf5e401f3
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-05 17:13:16 -0400

    first employee card working

--- develop/_tests_/Employee.test.js
+++ develop/_tests_/Employee.test.js
@@ -2,17 +2,17 @@ const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
 	it('should return name', () => {
-	const newEmployee = new Employee('John', '1', 'test@gmail.com')
-	expect(newEmployee.getName()).toBe('John')
+		const newEmployee = new Employee('John', '1', 'test@gmail.com')
+		expect(newEmployee.getName()).toBe('John')
 	});
 
 	it('should return id' , () => {
-	const newEmployee = new Employee('John', '1', 'test@gmail.com')
-	expect(newEmployee.getId()).toBe('1')
+		const newEmployee = new Employee('John', '1', 'test@gmail.com')
+		expect(newEmployee.getId()).toBe('1')
 	});
 
 	it('should return email' , () => {
-	const newEmployee = new Employee('John', '1', 'test@gmail.com')
-	expect(newEmployee.getEmail()).toBe('test@gmail.com')
+		const newEmployee = new Employee('John', '1', 'test@gmail.com')
+		expect(newEmployee.getEmail()).toBe('test@gmail.com')
 	});
 })
